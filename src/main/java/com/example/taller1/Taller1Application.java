38599c90eeaed89891da751f0eabc70ecea1c657
jaynAp1 <jerxonmjair@gmail.com>
2025-05-25 20:54:26 -0600

    railway-test

--- src/main/java/com/example/taller1/Taller1Application.java
+++ src/main/java/com/example/taller1/Taller1Application.java
@@ -2,8 +2,12 @@ package com.example.taller1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.taller1.infraestructure.repositorys")
+@EntityScan("com.example.taller1.domain")
 public class Taller1Application {
 
 	public static void main(String[] args) {
